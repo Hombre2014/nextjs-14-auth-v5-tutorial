0d620617bced9567f4e379844f99969b39c4996c
Yuriy Chamkoriyski <chamkoriyski@gmail.com>
2025-03-22 13:58:10 +0100

    fix: make sure user.id is available

--- actions/settings.ts
+++ actions/settings.ts
@@ -1,29 +1,27 @@
-"use server";
+'use server';
 
-import * as z from "zod";
-import bcrypt from "bcryptjs";
+import * as z from 'zod';
+import bcrypt from 'bcryptjs';
 
-import { update } from "@/auth";
-import { db } from "@/lib/db";
-import { SettingsSchema } from "@/schemas";
-import { getUserByEmail, getUserById } from "@/data/user";
-import { currentUser } from "@/lib/auth";
-import { generateVerificationToken } from "@/lib/tokens";
-import { sendVerificationEmail } from "@/lib/mail";
+import { update } from '@/auth';
+import { db } from '@/lib/db';
+import { SettingsSchema } from '@/schemas';
+import { getUserByEmail, getUserById } from '@/data/user';
+import { currentUser } from '@/lib/auth';
+import { generateVerificationToken } from '@/lib/tokens';
+import { sendVerificationEmail } from '@/lib/mail';
 
-export const settings = async (
-  values: z.infer<typeof SettingsSchema>
-) => {
+export const settings = async (values: z.infer<typeof SettingsSchema>) => {
   const user = await currentUser();
 
   if (!user) {
-    return { error: "Unauthorized" }
+    return { error: 'Unauthorized' };
   }
 
-  const dbUser = await getUserById(user.id);
+  const dbUser = await getUserById(user.id!);
 
   if (!dbUser) {
-    return { error: "Unauthorized" }
+    return { error: 'Unauthorized' };
   }
 
   if (user.isOAuth) {
@@ -37,34 +35,29 @@ export const settings = async (
     const existingUser = await getUserByEmail(values.email);
 
     if (existingUser && existingUser.id !== user.id) {
-      return { error: "Email already in use!" }
+      return { error: 'Email already in use!' };
     }
 
-    const verificationToken = await generateVerificationToken(
-      values.email
-    );
+    const verificationToken = await generateVerificationToken(values.email);
     await sendVerificationEmail(
       verificationToken.email,
-      verificationToken.token,
+      verificationToken.token
     );
 
-    return { success: "Verification email sent!" };
+    return { success: 'Verification email sent!' };
   }
 
   if (values.password && values.newPassword && dbUser.password) {
     const passwordsMatch = await bcrypt.compare(
       values.password,
-      dbUser.password,
+      dbUser.password
     );
 
     if (!passwordsMatch) {
-      return { error: "Incorrect password!" };
+      return { error: 'Incorrect password!' };
     }
 
-    const hashedPassword = await bcrypt.hash(
-      values.newPassword,
-      10,
-    );
+    const hashedPassword = await bcrypt.hash(values.newPassword, 10);
     values.password = hashedPassword;
     values.newPassword = undefined;
   }
@@ -73,7 +66,7 @@ export const settings = async (
     where: { id: dbUser.id },
     data: {
       ...values,
-    }
+    },
   });
 
   update({
@@ -82,8 +75,8 @@ export const settings = async (
       email: updatedUser.email,
       isTwoFactorEnabled: updatedUser.isTwoFactorEnabled,
       role: updatedUser.role,
-    }
+    },
   });
 
-  return { success: "Settings Updated!" }
-}
+  return { success: 'Settings Updated!' };
+};
